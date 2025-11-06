9979da791aca99d99a092bfa2fdc524b56f30854
JuanDavid-RodriguezBran <rodriguezjuandavid096@gmail.com>
2025-11-05 19:42:52 -0500

    pequeños cambios

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,11 +14,11 @@
             @yield('content')
         </main>
 
-       {{-- @include('layouts.footer')
+        @include('layouts.footer')
 
         <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
-        @include('layouts.scripts')--}}
+        @include('layouts.scripts')
     </body>
 
 </html>
---
+++ resources/views/layouts/scripts.blade.php
@@ -0,0 +1,48 @@
+<!-- Vendor JS Files -->
+<script src="{{ asset('assets/vendor/apexcharts/apexcharts.min.js') }}"></script>
+<script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('assets/vendor/chart.js/chart.umd.js') }}"></script>
+<script src="{{ asset('assets/vendor/echarts/echarts.min.js') }}"></script>
+<script src="{{ asset('assets/vendor/quill/quill.min.js') }}"></script>
+<script src="{{ asset('assets/vendor/simple-datatables/simple-datatables.js') }}"></script>
+<script src="{{ asset('assets/vendor/tinymce/tinymce.min.js') }}"></script>
+<script src="{{ asset('assets/vendor/php-email-form/validate.js') }}"></script>
+
+<!-- Template Main JS File -->
+<script src="{{ asset('assets/js/main.js') }}"></script>
+
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+@if(session()->has('message'))
+
+    <script>
+
+        const message = @json(session('message'));
+
+        Swal.fire({
+            title: message.content,
+            icon: message.type,
+        });
+
+    </script>
+
+@endif
+
+
+@if($errors->any())
+
+    {{-- <h1>{{ $errors }}</h1> --}}
+
+    <script>
+
+        Swal.fire({
+            title: 'Error',
+            icon: 'error',
+            html: `<ul> @foreach ($errors as $error) <li class="alert alert-danger">{{ $error }}</li>  @endforeach </ul>`
+        });
+
+    </script>
+
+@endif
